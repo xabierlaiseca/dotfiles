35fcdf73b02904cc6db472af79ac9ba4bbff55fe
xabier.laiseca <xabier.laiseca@gamesys.co.uk>
2015-05-27 15:32:01 +0100

    alias for python simple http server

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## docker
+
 MONGO_CONTAINER_NAME=mongo
 alias mongo_start="docker run -dP -p 27017:27017 --name=$MONGO_CONTAINER_NAME mongo:2.6.7"
 alias mongo_stop="docker rm -vf $MONGO_CONTAINER_NAME"
@@ -9,3 +11,6 @@ alias apigee_start="docker run -dP -p 18080:8080 -p 19000:9000 -p 19001:9001 --n
 alias apigee_stop="docker rm -vf $APIGEE_CONTAINER_NAME"
 
 alias docker_clean='docker rm -vf $(docker ps -aq)'
+
+## other
+alias simplehttp="python -m SimpleHTTPServer"
