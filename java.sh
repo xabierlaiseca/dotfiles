ccaa511b1420be24ade908ad8ce3c68e1320a908
xabier.laiseca <xabier.laiseca@gamesys.co.uk>
2015-06-23 15:34:18 +0100

    support for changing java version to 7 and 8

--- java.sh
+++ java.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 export MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128M"
-export JAVA_HOME=`/usr/libexec/java_home -v 1.7 2>/dev/null`
+
+function java-set-version {
+   ## $1 -> java version (example: 1.7)
+   export JAVA_HOME=`/usr/libexec/java_home -v $1 2>/dev/null`
+}
+
+alias java-set-version-7='java-set-version 1.7'
+alias java-set-version-8='java-set-version 1.8'
+
+java-set-version-8
+
