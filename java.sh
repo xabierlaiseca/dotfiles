5eca0fad0bf0d9fe4f9b4d17b9e87e68a02786eb
xabier.laiseca <xabier.laiseca@gamesys.co.uk>
2015-11-18 13:55:08 +0000

    java version added to prompt

--- java.sh
+++ java.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-export MAVEN_OPTS="-Xmx512m"
+export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=512m"
 
 alias mvnDebug='MAVEN_OPTS="$MAVEN_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005" mvn'
+
+function java-print-version {
+  echo $(java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/Java\2/; 1q')
+}
 
 function java-set-version {
    ## $1 -> java version (example: 1.7)
@@ -13,4 +17,3 @@ alias java-set-version-7='java-set-version 1.7'
 alias java-set-version-8='java-set-version 1.8'
 
 java-set-version-8
-
