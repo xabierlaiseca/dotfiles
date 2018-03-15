eae4f6e8867f7f423106f65fb85feb9795c45e67
Xabier Laiseca <xabier.laiseca@schibsted.com>
2018-03-15 10:28:53 +0000

    support activate virtual envs by name

--- python.sh
+++ python.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 VIRTUAL_ENVS_DIR="$HOME/virtualenvs"
-VIRTUAL_ENV_METADATA_DIR="$VIRTUAL_ENVS_DIR/.metadata"
-VIRTUAL_ENV_METADATA_NAME_FILE="name"
 
 function venv-create {
   local venv_name=${1:-$(basename $PWD)}
@@ -16,25 +14,61 @@ function venv-create {
 
   mkdir -p $VIRTUAL_ENVS_DIR
   (cd $VIRTUAL_ENVS_DIR && virtualenv "$prompt_arg" $venv_name)
-
-  local metadata_file_dir="$VIRTUAL_ENV_METADATA_DIR/$PWD"
-  mkdir -p $metadata_file_dir
-  echo $venv_name > "$metadata_file_dir/$VIRTUAL_ENV_METADATA_NAME_FILE"
 }
 
 function venv-activate {
-  local dir_to_check=$PWD
+  if [ -n "$1" ]; then
+    __venv_activate_by_name $1
+  else
+    __venv_activate_from_pwd
+  fi
+}
 
-  while [[ "$dir_to_check" != "/" ]] &&
-        [ ! -f "$VIRTUAL_ENV_METADATA_DIR/$dir_to_check/$VIRTUAL_ENV_METADATA_NAME_FILE" ]; do
-    dir_to_check=`dirname $dir_to_check`
+function __venv_activate_by_name {
+  if [ -d $VIRTUAL_ENVS_DIR/$1 ]; then
+      local venv_name=$1
+  else
+    local potential_venv_names=`find $VIRTUAL_ENVS_DIR -maxdepth 1 -type d -name "*$1*"`
+    local potential_venv_count=`echo $potential_venv_names | wc -w`
+
+    if [ $potential_venv_count == "1" ]; then
+      local venv_name=`echo $potential_venv_names | head -n 1 | xargs basename`
+    elif [ $potential_venv_count == "0" ]; then
+      echo "Could not find virtual environment"
+      return 1
+    else
+      echo "Multiple virtual environments found"
+
+      local name=""
+      for name in $potential_venv_names; do
+        echo " * $name"
+      done
+
+      return 1
+    fi
+  fi
+
+  source $VIRTUAL_ENVS_DIR/$venv_name/bin/activate
+}
+
+function __venv_activate_from_pwd {
+  local dir_to_check=$PWD
+  local venv_name=""
+
+  while [ "$dir_to_check" != "/" ] && [ -z $venv_name ]; do
+    local venv_name_to_check=`basename $dir_to_check`
+
+    if [ -d $VIRTUAL_ENVS_DIR/$venv_name_to_check ]; then
+      venv_name=$venv_name_to_check
+    else
+      dir_to_check=`dirname $dir_to_check`
+    fi
   done
 
-  if [[ "$dir_to_check" == "/" ]]; then
+  if [ -n $venv_name ]; then
+    source $VIRTUAL_ENVS_DIR/$venv_name/bin/activate
+  else
     echo "Could not find virtual environment"
     return 1
-  else
-    local venv_name=`cat "$VIRTUAL_ENV_METADATA_DIR/$dir_to_check/$VIRTUAL_ENV_METADATA_NAME_FILE"`
-    source $VIRTUAL_ENVS_DIR/$venv_name/bin/activate
   fi
 }
