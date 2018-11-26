4640a92209694860c42b166750ef7e30bd3a8c33
Xabier Laiseca <xabier.laiseca@schibsted.com>
2018-11-26 11:08:25 +0000

    jvm prompt for gradle & sbt

--- java.sh
+++ java.sh
@@ -4,10 +4,10 @@ export MAVEN_OPTS="-Xmx2048m -XX:MaxPermSize=512m"
 
 alias mvnDebug='MAVEN_OPTS="$MAVEN_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005" mvn'
 
-function __java_for_prompt {
-  if [[ -n $(upsearch "pom.xml") ]]; then
-    java_version=$(java -version 2>&1 | sed 's/\(openjdk\|java\) version "\(.*\)\.\(.*\)\..*"/Java\3/; 1q')
-    printf -- "$1" "$java_version"
+function __jvm_for_prompt {
+  if [[ -n $(upsearch "pom.xml") ]] || [[ -n $(upsearch "build.gradle") ]] || [[ -n $(upsearch "build.sbt") ]]; then
+    java_version=$(java -version 2>&1 | head -n 1 | sed 's/.*"\(.*\)"/\1/')
+    printf -- "$1" "jvm $java_version"
   fi
 }
 
--- shell.sh
+++ shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $DETECTED_OS == $OS_OSX ]]; then
-  export PS1="\h:\[$Green\]\w\[$Color_Off\]\$(__java_for_prompt ' (\[$IRed\]%s\[$Color_Off\])')\$(__git_ps1 ' (\[$IRed\]%s\[$Color_Off\])') \u\$ "
+  export PS1="\h:\[$Green\]\w\[$Color_Off\]\$(__jvm_for_prompt ' (\[$IRed\]%s\[$Color_Off\])')\$(__git_ps1 ' (\[$IRed\]%s\[$Color_Off\])') \u\$ "
 elif [[ $DETECTED_OS == $OS_LINUX ]]; then
-  export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\[$Green\]\w\[$Color_Off\]\$(__java_for_prompt ' (\[$IRed\]%s\[$Color_Off\])')\$(__git_ps1 ' (\[$IRed\]%s\[$Color_Off\])') \$ "
+  export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\u@\h:\[$Green\]\w\[$Color_Off\]\$(__jvm_for_prompt ' (\[$IRed\]%s\[$Color_Off\])')\$(__git_ps1 ' (\[$IRed\]%s\[$Color_Off\])') \$ "
 fi
