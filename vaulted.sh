3f6f7ce19e94caedc664e46b56a7f19927a3b62d
Xabier Laiseca <xabier.laiseca@schibsted.com>
2019-01-28 10:56:16 +0000

    enabling vaulted prompt only if command exists

--- vaulted.sh
+++ vaulted.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-function __vaulted_for_prompt {
-  if [[ -n "$VAULTED_ENV" ]]; then
-    printf -- "$1" "vaulted $VAULTED_ENV"
-  fi
-}
+if [ -n "$(command -v vaulted 2>/dev/null)" ]; then
+  function __vaulted_for_prompt {
+    if [[ -n "$VAULTED_ENV" ]]; then
+      printf -- "$1" "vaulted $VAULTED_ENV"
+    fi
+  }
 
-PROMPT_SECTIONS+=("__vaulted_for_prompt")
+  PROMPT_SECTIONS+=("__vaulted_for_prompt")
+fi
