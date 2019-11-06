7e24823859c6d716bcbcee80295e00aefb0858c8
Xabier Laiseca <xlaiseca@Xabier-Laisecas-MBP15.local>
2019-11-06 12:08:28 +0000

    updated homebrew completions

--- homebrew.sh
+++ homebrew.sh
@@ -2,8 +2,15 @@
 
 if [[ $DETECTED_OS == $OS_OSX ]]; then
 
-  if [ -f `brew --prefix`/etc/bash_completion ]; then
-      . `brew --prefix`/etc/bash_completion
+  if type brew &>/dev/null; then
+    HOMEBREW_PREFIX="$(brew --prefix)"
+    if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
+      source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+    else
+      for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+        [[ -r "$COMPLETION" ]] && source "$COMPLETION"
+      done
+    fi
   fi
 
 fi
